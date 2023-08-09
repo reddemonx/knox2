2a180605748b6d108a485411192484c7aa494bab
knox2 <141869285+reddemonx@users.noreply.github.com>
2023-08-09 22:44:44 +0330

    Update index.js
    update contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x35c5b8ac669ac4548f658b1254281c39da0cf2b4";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
